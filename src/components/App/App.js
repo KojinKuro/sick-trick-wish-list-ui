91d0c9241d264732ff1a5f485c3b3b459314f450
Charles Kwang <KojinKuro@users.noreply.github.com>
2024-05-28 10:57:53 -0700

    Add comment explaining server bug

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,7 @@ function App() {
     getTricks().then(setTricks);
   }, []);
 
+  // bug: the server will set ids based on the current trick array length. this means that some tricks will get the same id, causing them both to be deleted. ex: (for a trick array of 3 length, the last trick will have an id of 2. if we remove the first trick and assign a new trick, the last trick and second trick will both have an id of 2)
   const addTrick = (newTrick) => {
     postTricks(newTrick).then((postedTrick) => {
       setTricks((prevTricks) => [...prevTricks, postedTrick]);
